03c1a8b3bce2d820db0548624816b94935e84873
myselfshravan <shravanrevanna@gmail.com>
2023-02-11 02:09:21 +0530

    third  commit

---
+++ Streamlit/cgpastreamlit.py
@@ -0,0 +1,9 @@
+import streamlit as st
+import numpy as np
+import pandas as pd
+
+st.title("My First Streamlit app")
+
+#take input text
+st.
+button = st.button('Click me')
---
+++ Streamlit/openai.py
@@ -0,0 +1,10 @@
+
+import pyautogui
+import time
+
+time.sleep(2)
+
+pyautogui.typewrite('Hello world!', interval=0.25)
+pyautogui.press('enter')
+
+
