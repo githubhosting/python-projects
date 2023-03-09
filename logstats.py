d01b7520666659d2e416b91f447fd369eb103f15
myselfshravan <shravanrevanna@gmail.com>
2023-03-09 12:16:15 +0530

    made changes to log stats

--- logstats.py
+++ logstats.py
@@ -5,6 +5,7 @@ from datetime import datetime
 time_frame = []
 all_usns = []
 tokens = []
+
 with open('logs.log', 'r') as f:
 	for _ in range(5): st.write("\n")
 	line = f.readline()
@@ -21,13 +22,23 @@ with open('logs.log', 'r') as f:
 			time_frame.append(time_)
 	df = pd.DataFrame({"Time": time_frame}, index=range(1, len(time_frame) + 1))
 	dept = []
+	dept_ = []
+	unique_usn = list(set(all_usns))
 	for usn in all_usns:
-		dept.append(usn[6:8])
+		dept.append(usn[4:8])
+	for usn in unique_usn:
+		dept_.append(usn[4:8])
 	unique_dept = list(set(dept))
 	dept_count = []
+	dept_count_ = []
 	for i in unique_dept:
 		dept_count.append(dept.count(i))
-	dept_counts = pd.DataFrame({"Dept": unique_dept, "Count": dept_count})
+	for j in unique_dept:
+		dept_count_.append(dept_.count(j))
+
+	dept_count, dept_count_, unique_dept = zip(*sorted(zip(dept_count, dept_count_, unique_dept), reverse=True))
+
+	dept_counts = pd.DataFrame({"Dept": unique_dept, "Count Rep": dept_count, "Count Unique": dept_count_})
 	st.subheader("Time wise count")
 	st.line_chart(df, use_container_width=True)
 	st.subheader("Department wise count")
